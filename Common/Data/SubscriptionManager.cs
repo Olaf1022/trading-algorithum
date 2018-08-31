Martin-Molinero/Lean

Create DataManager class

Common/Data/SubscriptionManager.cs
@@ -14,7 +14,6 @@
 */
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using NodaTime;
@@ -34,15 +33,12 @@ namespace QuantConnect.Data
     {
         private readonly IAlgorithmSettings _algorithmSettings;
         private readonly TimeKeeper _timeKeeper;
-
-        /// There is no ConcurrentHashSet collection in .NET,
-        /// so we use ConcurrentDictionary with byte value to minimize memory usage
-        private readonly ConcurrentDictionary<SubscriptionDataConfig, byte> _subscriptions = new ConcurrentDictionary<SubscriptionDataConfig, byte>();
+        private IDataManager _dataManager;
 
         /// <summary>
         /// Returns an IEnumerable of Subscriptions
         /// </summary>
-        public IEnumerable<SubscriptionDataConfig> Subscriptions => _subscriptions.Select(x => x.Key);
+        public IEnumerable<SubscriptionDataConfig> Subscriptions => _dataManager.Subscriptions;
 
         /// <summary>
         /// Flags the existence of custom data in the subscriptions
@@ -71,7 +67,7 @@ namespace QuantConnect.Data
         /// <summary>
         /// Get the count of assets:
         /// </summary>
-        public int Count => _subscriptions.Skip(0).Count();
+        public int Count => _dataManager.Count();
 
         /// <summary>
         /// Add Market Data Required (Overloaded method for backwards compatibility).
@@ -128,13 +124,13 @@ namespace QuantConnect.Data
             var newConfig = new SubscriptionDataConfig(dataType, symbol, resolution, dataTimeZone, exchangeTimeZone, fillDataForward, extendedMarketHours, isInternalFeed, isCustomData, isFilteredSubscription: isFilteredSubscription, tickType: tickType);
 
             //Add to subscription list: make sure we don't have this symbol:
-            if (_subscriptions.ContainsKey(newConfig))
+            if (_dataManager.ContainsKey(newConfig))
             {
                 Log.Trace("SubscriptionManager.Add(): subscription already added: " + newConfig);
                 return newConfig;
             }
 
-            _subscriptions.TryAdd(newConfig, 0);
+            _dataManager.TryAdd(newConfig);
 
             // count data subscriptions by symbol, ignoring multiple data types
             var uniqueCount = Subscriptions
@@ -247,5 +243,12 @@ namespace QuantConnect.Data
             return AvailableDataTypes[symbolSecurityType].Select(tickType => new Tuple<Type, TickType>(LeanData.GetDataType(resolution, tickType), tickType)).ToList();
         }
 
+        /// <summary>
+        /// Sets the data manager
+        /// </summary>
+        public void SetDataManager(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
     }
 }

Common/Interfaces/IDataManager.cs
@@ -1,0 +1,48 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System.Collections.Generic;
+using QuantConnect.Data;
+
+namespace QuantConnect.Interfaces
+{
+    /// <summary>
+    /// Subscription handler interface
+    /// </summary>
+    public interface IDataManager
+    {
+        /// <summary>
+        /// Gets all the current data config subscriptions that are being processed
+        /// </summary>
+        IEnumerable<SubscriptionDataConfig> Subscriptions { get; }
+
+        /// <summary>
+        /// Adds a new subscription data config
+        /// </summary>
+        /// <returns>True, if the configuration was added successfully</returns>
+        bool TryAdd(SubscriptionDataConfig config);
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        bool ContainsKey(SubscriptionDataConfig config);
+
+        /// <summary>
+        /// Returns the amount of data config subscriptions processed
+        /// </summary>
+        int Count();
+    }
+}

Engine/DataFeeds/DataManager.cs
@@ -1,0 +1,69 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+
+namespace QuantConnect.Lean.Engine.DataFeeds
+{
+    /// <summary>
+    /// DataManager will manage the subscriptions for both the DataFeeds and the SubscriptionManager
+    /// </summary>
+    public class DataManager : IDataManager
+    {
+        /// There is no ConcurrentHashSet collection in .NET,
+        /// so we use ConcurrentDictionary with byte value to minimize memory usage
+        private readonly ConcurrentDictionary<SubscriptionDataConfig, byte> _subscriptionManagerSubscriptions
+            = new ConcurrentDictionary<SubscriptionDataConfig, byte>();
+
+        /// <summary>
+        /// Gets the data feed subscription collection
+        /// </summary>
+        public readonly SubscriptionCollection DataFeedSubscriptions = new SubscriptionCollection();
+
+        /// <summary>
+        /// Gets all the current data config subscriptions that are being processed
+        /// </summary>
+        public IEnumerable<SubscriptionDataConfig> Subscriptions => _subscriptionManagerSubscriptions.Select(x => x.Key);
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        public bool TryAdd(SubscriptionDataConfig config)
+        {
+            return _subscriptionManagerSubscriptions.TryAdd(config, 0);
+        }
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        public bool ContainsKey(SubscriptionDataConfig config)
+        {
+            return _subscriptionManagerSubscriptions.ContainsKey(config);
+        }
+
+        /// <summary>
+        /// Returns the amount of data config subscriptions processed
+        /// </summary>
+        public int Count()
+        {
+            return _subscriptionManagerSubscriptions.Skip(0).Count();
+        }
+    }
+}

Olaf1022/trading-algorithum

Common/Data/SubscriptionManager.cs
@@ -14,7 +14,6 @@
 */
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using NodaTime;
@@ -34,15 +33,12 @@ namespace QuantConnect.Data
     {
         private readonly IAlgorithmSettings _algorithmSettings;
         private readonly TimeKeeper _timeKeeper;
-
-        /// There is no ConcurrentHashSet collection in .NET,
-        /// so we use ConcurrentDictionary with byte value to minimize memory usage
-        private readonly ConcurrentDictionary<SubscriptionDataConfig, byte> _subscriptions = new ConcurrentDictionary<SubscriptionDataConfig, byte>();
+        private IDataManager _dataManager;
 
         /// <summary>
         /// Returns an IEnumerable of Subscriptions
         /// </summary>
-        public IEnumerable<SubscriptionDataConfig> Subscriptions => _subscriptions.Select(x => x.Key);
+        public IEnumerable<SubscriptionDataConfig> Subscriptions => _dataManager.SubscriptionManagerSubscriptions;
 
         /// <summary>
         /// Flags the existence of custom data in the subscriptions
@@ -71,7 +67,7 @@ namespace QuantConnect.Data
         /// <summary>
         /// Get the count of assets:
         /// </summary>
-        public int Count => _subscriptions.Skip(0).Count();
+        public int Count => _dataManager.SubscriptionManagerCount();
 
         /// <summary>
         /// Add Market Data Required (Overloaded method for backwards compatibility).
@@ -128,13 +124,13 @@ namespace QuantConnect.Data
             var newConfig = new SubscriptionDataConfig(dataType, symbol, resolution, dataTimeZone, exchangeTimeZone, fillDataForward, extendedMarketHours, isInternalFeed, isCustomData, isFilteredSubscription: isFilteredSubscription, tickType: tickType);
 
             //Add to subscription list: make sure we don't have this symbol:
-            if (_subscriptions.ContainsKey(newConfig))
+            if (_dataManager.SubscriptionManagerContainsKey(newConfig))
             {
                 Log.Trace("SubscriptionManager.Add(): subscription already added: " + newConfig);
                 return newConfig;
             }
 
-            _subscriptions.TryAdd(newConfig, 0);
+            _dataManager.SubscriptionManagerTryAdd(newConfig);
 
             // count data subscriptions by symbol, ignoring multiple data types
             var uniqueCount = Subscriptions
@@ -247,5 +243,12 @@ namespace QuantConnect.Data
             return AvailableDataTypes[symbolSecurityType].Select(tickType => new Tuple<Type, TickType>(LeanData.GetDataType(resolution, tickType), tickType)).ToList();
         }
 
+        /// <summary>
+        /// Sets the data manager
+        /// </summary>
+        public void SetDataManager(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
     }
 }

Common/Interfaces/IDataManager.cs
@@ -1,0 +1,48 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System.Collections.Generic;
+using QuantConnect.Data;
+
+namespace QuantConnect.Interfaces
+{
+    /// <summary>
+    /// Subscription handler interface
+    /// </summary>
+    public interface IDataManager
+    {
+        /// <summary>
+        /// Gets all the current data config subscriptions that are being processed
+        /// </summary>
+        IEnumerable<SubscriptionDataConfig> SubscriptionManagerSubscriptions { get; }
+
+        /// <summary>
+        /// Adds a new subscription data config
+        /// </summary>
+        /// <returns>True, if the configuration was added successfully</returns>
+        bool SubscriptionManagerTryAdd(SubscriptionDataConfig config);
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        bool SubscriptionManagerContainsKey(SubscriptionDataConfig config);
+
+        /// <summary>
+        /// Returns the amount of data config subscriptions processed
+        /// </summary>
+        int SubscriptionManagerCount();
+    }
+}

Engine/DataFeeds/DataManager.cs
@@ -1,0 +1,69 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+
+namespace QuantConnect.Lean.Engine.DataFeeds
+{
+    /// <summary>
+    /// DataManager will manage the subscriptions for both the DataFeeds and the SubscriptionManager
+    /// </summary>
+    public class DataManager : IDataManager
+    {
+        /// There is no ConcurrentHashSet collection in .NET,
+        /// so we use ConcurrentDictionary with byte value to minimize memory usage
+        private readonly ConcurrentDictionary<SubscriptionDataConfig, byte> _subscriptionManagerSubscriptions
+            = new ConcurrentDictionary<SubscriptionDataConfig, byte>();
+
+        /// <summary>
+        /// Gets the data feed subscription collection
+        /// </summary>
+        public readonly SubscriptionCollection DataFeedSubscriptions = new SubscriptionCollection();
+
+        /// <summary>
+        /// Gets all the current data config subscriptions that are being processed
+        /// </summary>
+        public IEnumerable<SubscriptionDataConfig> SubscriptionManagerSubscriptions => _subscriptionManagerSubscriptions.Select(x => x.Key);
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        public bool SubscriptionManagerTryAdd(SubscriptionDataConfig config)
+        {
+            return _subscriptionManagerSubscriptions.TryAdd(config, 0);
+        }
+
+        /// <summary>
+        /// Returns true if the given subscription data config is already present
+        /// </summary>
+        public bool SubscriptionManagerContainsKey(SubscriptionDataConfig config)
+        {
+            return _subscriptionManagerSubscriptions.ContainsKey(config);
+        }
+
+        /// <summary>
+        /// Returns the amount of data config subscriptions processed
+        /// </summary>
+        public int SubscriptionManagerCount()
+        {
+            return _subscriptionManagerSubscriptions.Skip(0).Count();
+        }
+    }
+}