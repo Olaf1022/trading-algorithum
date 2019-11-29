QuantConnect/Lean

Address reviews
- Rename history provider parameters `LiveMode` to `ParallelHistoryRequestsEnabled`
- Reduce code duplication

Engine/HistoricalData/SubscriptionDataReaderHistoryProvider.cs
@@ -41,7 +41,7 @@ namespace QuantConnect.Lean.Engine.HistoricalData
         private IMapFileProvider _mapFileProvider;
         private IFactorFileProvider _factorFileProvider;
         private IDataCacheProvider _dataCacheProvider;
-        private bool _liveMode;
+        private bool _parallelHistoryRequestsEnabled;
 
         /// <summary>
         /// Initializes this history provider to work for the specified job
@@ -52,7 +52,7 @@ namespace QuantConnect.Lean.Engine.HistoricalData
             _mapFileProvider = parameters.MapFileProvider;
             _factorFileProvider = parameters.FactorFileProvider;
             _dataCacheProvider = parameters.DataCacheProvider;
-            _liveMode = parameters.LiveMode;
+            _parallelHistoryRequestsEnabled = parameters.ParallelHistoryRequestsEnabled;
         }
 
         /// <summary>
@@ -170,12 +170,11 @@ namespace QuantConnect.Lean.Engine.HistoricalData
             });
             var subscriptionRequest = new SubscriptionRequest(false, null, security, config, request.StartTimeUtc, request.EndTimeUtc);
 
-            if (_liveMode)
+            if (_parallelHistoryRequestsEnabled)
             {
-                // for live mode we wont use workers due to their resource consumption, specifically ram
-                return SubscriptionUtils.Create(subscriptionRequest, dataReader);
+                return SubscriptionUtils.CreateAndScheduleWorker(subscriptionRequest, reader, 0, int.MaxValue);
             }
-            return SubscriptionUtils.CreateAndScheduleWorker(subscriptionRequest, reader, 0, int.MaxValue);
+            return SubscriptionUtils.Create(subscriptionRequest, dataReader);
         }
 
         private class FilterEnumerator<T> : IEnumerator<T>

Olaf1022/trading-algorithum

Engine/HistoricalData/SubscriptionDataReaderHistoryProvider.cs
@@ -41,7 +41,7 @@ namespace QuantConnect.Lean.Engine.HistoricalData
         private IMapFileProvider _mapFileProvider;
         private IFactorFileProvider _factorFileProvider;
         private IDataCacheProvider _dataCacheProvider;
-        private bool _liveMode;
+        private bool _parallelHistoryRequestsEnabled;
 
         /// <summary>
         /// Initializes this history provider to work for the specified job
@@ -52,7 +52,7 @@ namespace QuantConnect.Lean.Engine.HistoricalData
             _mapFileProvider = parameters.MapFileProvider;
             _factorFileProvider = parameters.FactorFileProvider;
             _dataCacheProvider = parameters.DataCacheProvider;
-            _liveMode = parameters.LiveMode;
+            _parallelHistoryRequestsEnabled = parameters.ParallelHistoryRequestsEnabled;
         }
 
         /// <summary>
@@ -170,12 +170,11 @@ namespace QuantConnect.Lean.Engine.HistoricalData
             });
             var subscriptionRequest = new SubscriptionRequest(false, null, security, config, request.StartTimeUtc, request.EndTimeUtc);
 
-            if (_liveMode)
+            if (_parallelHistoryRequestsEnabled)
             {
-                // for live mode we wont use workers due to their resource consumption, specifically ram
-                return SubscriptionUtils.Create(subscriptionRequest, reader);
+                return SubscriptionUtils.CreateAndScheduleWorker(subscriptionRequest, reader);
             }
-            return SubscriptionUtils.CreateAndScheduleWorker(subscriptionRequest, reader);
+            return SubscriptionUtils.Create(subscriptionRequest, reader);
         }
 
         private class FilterEnumerator<T> : IEnumerator<T>